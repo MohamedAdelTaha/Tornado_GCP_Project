de4c0f8ec3338de2c8f1c0d0121133cbea7fbe3f
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-10-31 22:28:14 +0200

    Add GKE cluster

---
+++ Terraform_Section/modules/GKE_Cluster/GKE.tf
@@ -0,0 +1,30 @@
+resource "google_container_cluster" "private-cluster" {
+  name                     = "private-cluster"
+  location                 = "us-central1-a"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  network                  = google_compute_network.vpc_network.id
+  subnetwork               = google_compute_subnetwork.restricted-subnet.id
+
+  node_locations = [
+    "us-central1-b"
+  ]
+
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.0/24"
+      display_name = "managment-cidr-range"
+    }
+  }
+
+  ip_allocation_policy {
+
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+
+}
